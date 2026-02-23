a478d996bd4c31b6b10e2e7d5d84f734b4f687f6
faridasyraff <mzm7127@psu.edu>
2026-02-23 16:22:28 -0500

    feat: more visual improvement on navbar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -21,11 +21,11 @@ function Navbar() {
 
             <ul className={`nav-links ${isOpen ? "open" : ""}`}>
                 <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
-                <li><Link to="/atlas" onClick={() => setIsOpen(false)}>Atlas</Link></li>
-                <li><Link to="/atlas-2d" onClick={() => setIsOpen(false)}>2D Atlas (MVP)</Link></li>
-                <li><Link to="/data" onClick={() => setIsOpen(false)}>Query Data</Link></li>
-                <li><Link to="/ai-prompt" onClick={() => setIsOpen(false)}>AI Prompt</Link></li>
                 <li><Link to="/2D-brain" onClick={() => setIsOpen(false)}>2D Brain</Link></li>
+                <li><Link to="/atlas" onClick={() => setIsOpen(false)}>Atlas</Link></li>
+                {/*<li><Link to="/atlas-2d" onClick={() => setIsOpen(false)}>2D Atlas (MVP)</Link></li>*/}
+                <li><Link to="/data" onClick={() => setIsOpen(false)}>Wordcloud</Link></li>
+                {/*<li><Link to="/ai-prompt" onClick={() => setIsOpen(false)}>AI Prompt</Link></li>*/}
             </ul>
         </nav>
     );
