5cf15c7c18e521d9f8c0226b01a2ee9d03bf3f09
faridasyraff <mzm7127@psu.edu>
2026-03-01 10:33:08 -0500

    feat: minor visual improvement

--- frontend/src/app/Layout.jsx
+++ frontend/src/app/Layout.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar.jsx";
 
 function Layout() {
-  return (
-    <div className="App">
-      <Navbar />
-      <main className="main-content">
-        <Outlet />
-      </main>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-950">
+            <Navbar />
+            <main className="flex-1">
+                <Outlet />
+            </main>
+        </div>
+    );
 }
 
 export default Layout;
